e271ecae8f2d88c88ff7b6dcb30ccd98a5c94f8f
1molPotato <lky.hoooo94@gmail.com>
2017-12-13 16:18:16 +0800

    Update 编程题#1.cpp
    利用STL标准库中的string类中的模板函数简化代码

--- OperatorOverloading/编程题#1.cpp
+++ OperatorOverloading/编程题#1.cpp
@@ -1,70 +1,38 @@
 /*
 注意： 总时间限制: 1000ms 内存限制: 65536kB
-
 描述
-
 下面程序的输出是：
-
 3+4i
-
 5+6i
-
 请补足Complex类的成员函数。不能加成员变量。
 */
 
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <cstdlib>
-#include <cmath>//
 using namespace std;
 class Complex {
-private:    
-    double r,i;
-public:    
-    void Print() {
-        cout << r << "+" << i << "i" << endl;
-    }
-// 在此处补充你的代码
-    Complex(){
-        //无参构造函数，初始化实部和虚部为0
-        r = 0.0;
-        i = 0.0;
-    }
-    Complex & operator = (const string & str){
-	r = 0.0;
-	i = 0.0;
-	//实部和虚部的位数
-	int digit_r = 0;
-	int digit_i = 0;
-	char reals[10];
-	char imags[10];
-	//从str首元素开始遍历，直到遇到字符'+'，记录遍历过的元素reals[digit1]，表示实部的位数digit1
-	//for(int i = 0; i < digit1; i++)
-	//	r += (int)reals[i] * 10的(digit1 - i - 1)次方
-	int k;
-	for(k = 0; str[k] != '+'; k++) {
-		reals[k] = str[k];
-		digit_r++;
+private:
+	double r, i;
+public:
+	void Print() {
+		cout << r << "+" << i << "i" << endl;
 	}
-	for(int j = 0; j < digit_r; j++) {
-		r += atoi(&reals[j]) * pow(10, digit_r - j - 1);//atoi函数将字符型转化为整型
+	// 在此处补充你的代码
+	Complex & operator = (const string & str) {
+		int pos = str.find('+', 0);//寻找+号并返回它的位置
+		string str_real = str.substr(0, pos);//分离出代表实部的字符串
+		r = stod(str_real);//库函数stod将str_real中的内容转换成double类型
+		string str_imag = str.substr(pos + 1, str.size() - pos - 2);
+		i = stod(str_imag);
+
+		return *this;//返回成员函数所作用的对象
 	}
-	//从'+'后面的字符开始遍历，直到遇到字符'i'，记录遍历过的元素imags[digit2]，表示虚部的位数digit2
-	//for(int i = 0; i < digit2; i++)
-	//	i += (int)imags[i] * 10的(digit2 - i - 1)次方
-	for(; str[k + 1] != 'i'; k++) {
-		imags[k - digit_r] = str[k + 1];
-		digit_i++;
-	}
-	for(int j = 0; j < digit_r; j++) {
-		i += atoi(&imags[j]) * pow(10, digit_r - j - 1);//atoi函数将字符型转化为整型
-	}
-	return *this;//返回成员函数所作用的对象
-    }
 };
 int main() {
-    Complex a;
-    a = "3+4i"; a.Print();
-    a = "5+6i"; a.Print();
-    return 0;
+	Complex a;
+	a = "3+4i"; a.Print();
+	a = "5+6i"; a.Print();
+
+	return 0;
 }
