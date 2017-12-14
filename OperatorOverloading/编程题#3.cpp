9c86ba4e05ddb06cb836357e7b7096477df768e5
1molPotato <lky.hoooo94@gmail.com>
2017-12-14 18:23:51 +0800

    Create 编程题#3.cpp
    很傻的实现方式，数组大小预先给定，如果超出范围则会编译出错，应改进为动态生成数组

---
+++ OperatorOverloading/编程题#3.cpp
@@ -0,0 +1,74 @@
+/*
+注意： 总时间限制: 1000ms 内存限制: 65536kB
+
+描述
+
+写一个二维数组类 Array2,使得下面程序的输出结果是：
+
+0,1,2,3,
+
+4,5,6,7,
+
+8,9,10,11,
+
+next
+
+0,1,2,3,
+
+4,5,6,7,
+
+8,9,10,11,
+
+程序：
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+// 在此处补充你的代码
+class Array2 {
+private:
+	int array[100][100] = {};//内部数据为整型二维数组
+public:
+	//函数原型声明
+	Array2();//无参构造函数
+	Array2(int raw, int col);//构造函数
+	int* operator [](int index_raw);//重载[]运算符,返回第index_raw行的整型数组指针
+	int operator()(int index_raw, int index_col);//重载()运算符,返回整型数
+};
+
+Array2::Array2() {
+
+}
+Array2::Array2(int raw, int col) {
+	
+}
+int* Array2::operator[](int index_raw) {
+	return array[index_raw];
+}
+int Array2::operator()(int index_raw, int index_col) {
+	return array[index_raw][index_col];
+}
+
+int main() {
+	Array2 a(3, 4);
+	int i, j;
+	for(i = 0; i < 3; ++i)
+		for(j = 0; j < 4; j++)
+			a[i][j] = i * 4 + j;
+	for(i = 0; i < 3; ++i) {
+		for(j = 0; j < 4; j++) {
+			cout << a(i, j) << ",";
+		}
+		cout << endl;
+	}
+	cout << "next" << endl;
+	Array2 b; b = a;
+	for(i = 0; i < 3; ++i) {
+		for(j = 0; j < 4; j++) {
+			cout << b[i][j] << ",";
+		}
+		cout << endl;
+	}
+	return 0;
+}
