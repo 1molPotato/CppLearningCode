5072452daa824c95ff51d1cbd59fa417a1a3e19d
1molPotato <lky.hoooo94@gmail.com>
2018-01-18 15:51:10 +0800

    Update LongestPalindromicSubstring.cpp

--- LeetCode/LongestPalindromicSubstring.cpp
+++ LeetCode/LongestPalindromicSubstring.cpp
@@ -78,3 +78,45 @@ int expandAroundCenter(const string &s, int left, int right) {
 	}
 	return right - left - 1;
 }
+
+/*
+Dynamic Programming(动态规划)是指将原问题拆分成更易求解的子问题，并将求解子问题的结果记忆化存储，避免重复求解
+例如在对字符串"ababa"求解最长回文子串时，若已经求得"bab"是回文串，则"ababa"是否是回文串只取决于"a"和"a"是否相等
+具体实现可以用n * n大小的bool数组dp[][]来存储子问题的解，若s[i],s[i+1],...,s[j]是回文串，则dp[i][j] = true
+下面给出实现代码，AC的Runtime为99ms，感觉优化远远不够，有时间再分析一下原因
+*/
+
+string longestPalindrome(string s) {
+	int len = s.size();
+	// s.substr(i, j - i + 1) is a palindrome if dp[i][j] == true 
+	bool *dp = new bool[len * len];// dynamic memory allocation
+	
+	//initialize value in array
+	for(int k = 0; k < len * len; ++k) {
+		dp[k] = false;
+	}
+	
+	//int start record the begin position of lps
+	//int len_ls record the length of lps
+	int start = 0, len_ls = 0;
+	for(int i = 0; i < len; ++i) {
+		for(int j = 0; j < len - i; ++j) {
+			if(0 == i) {
+				// dp[i][i] = true;
+				dp[j * len + j] = true;
+			} else if(1 == i) {
+				// dp[i][i + 1] = (s[i] == s[i + 1])
+				dp[j * len + j + 1] = (s[j] == s[j + 1]); 
+			} else {
+				// dp[i][j] = (s[i] == s[j]) and dp[i + 1][j - 1]
+				dp[j * len + j + i] = (s[j] == s[j + i]) && dp[(j + 1) * len + j + i - 1];
+			}
+			if(dp[j * len + j + i] && len_ls < i + 1) {
+				start = j;
+				len_ls = i + 1;
+			}
+		}
+	}
+	delete[]dp;// release the memory allocated dynamicly
+	return s.substr(start, len_ls);
+}
