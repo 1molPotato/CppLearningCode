72bbb72a1a849e0d699849e73e140bd20c22a0db
1molPotato <lky.hoooo94@gmail.com>
2018-01-18 12:26:35 +0800

    Update LongestPalindromicSubstring.cpp

--- LeetCode/LongestPalindromicSubstring.cpp
+++ LeetCode/LongestPalindromicSubstring.cpp
@@ -20,6 +20,8 @@ Output: "bb"
 下面先给出中心扩展法的解，也是看了半天解题思路才写完最后AC的，时间复杂度是O(n^2)，接下来准备理解一下DP（动态规划）和马拉车算法
 */
 
+// Expand around center method
+// 回文串的特点是关于中心对称（偶数个数时中心在最中间两个字符之间），设字符串s长度为n，则共有2n-1个中心
 string longestPalindrome(string s) {
 	if(s.size() < 2) {
 		return s;
@@ -51,7 +53,28 @@ string longestPalindrome(string s) {
 		--right;
 		ls =  s.substr(left, right - left + 1).size() > ls.size() ? s.substr(left, right - left + 1) : ls;
 	}
-
-
 	return ls;
+}
+
+// 然后对比之后发现还是写得太丑了，整改如下，最终AC的Runtime为16ms
+
+string longestPalindrome(string s) {
+	int start = 0, len_ls = 0;
+	for(int i = 0; i < s.size(); ++i) {
+		int len1 = expandAroundCenter(s, i, i);
+		int len2 = expandAroundCenter(s, i, i + 1);
+		if(len_ls < max(len1, len2)) {
+			len_ls = max(len1, len2);
+			start = i - (len_ls - 1) / 2;
+		}
+	}
+	return s.substr(start, len_ls);
+}
+
+int expandAroundCenter(const string &s, int left, int right) {
+	while(left >= 0 && right < s.size() && s[left] == s[right]) {
+		--left;
+		++right;
+	}
+	return right - left - 1;
 }
