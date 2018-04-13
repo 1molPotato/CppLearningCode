b40652ae9f34c8d6f5226223d1d3c5d45eadb9f9
1molPotato <lky.hoooo94@gmail.com>
2018-04-13 13:06:55 +0800

    Update QuickSort.cpp

--- Algorithm/QuickSort.cpp
+++ Algorithm/QuickSort.cpp
@@ -2,10 +2,11 @@
 接下来是快排,先照着wiki上的伪码算法自己写了一个,非原地(in place)版本
 */
 
-// start of quick sort algorithm, non-in-place version, by myself
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+// start of quick sort algorithm, non-in-place version, by myself
 template<typename T>
 vector<T> joint(vector<T> &v1, vector<T> &v2, vector<T> &v3) {
 	vector<int> res;
@@ -29,4 +30,32 @@ vector<T> quick_sort(vector<T> &arr) {
 	return joint(quick_sort(less), pivotList, quick_sort(greater));
 }
 // end of quick sort algorithm, non-in-place version, by myself
+
+// start of quick sort algorithm, in-place version, by myself
+template<typename T>
+int partition(T arr[], int left, int right, int pivotIndex) {
+	T pivotValue = arr[pivotIndex];
+	swap(arr[pivotIndex], arr[right]); // move pivot to the tail
+	int storeIndex = left; // stored as pivotIndex to return
+	for(int i = left; i < right; ++i) {
+		// move element less than pivot to the left(not in order)
+		if(arr[i] <= pivotValue) swap(arr[storeIndex++], arr[i]);
+	}
+	swap(arr[right], arr[storeIndex]); // move pivot back to its place
+	return storeIndex;
+}
+
+template<typename T>
+void quick_sort(T arr[], int left, int right) {
+	if(right > left) {
+		// if right <= left, only 0 or 1 element, no need to sort
+		int pivotIndex = (right + left) / 2; // still don't know how to choose pivot
+		int pivotNewIndex = partition(arr, left, right, pivotIndex);
+		quick_sort(arr, left, pivotNewIndex - 1); // recursively call quick_sort for left part
+		quick_sort(arr, pivotNewIndex + 1, right); // similarly, for right part
+	}
+}
+// end of quick sort algorithm, in-place version, by myself
+
+// 原地版本, 但是pivot的选取仍未做考虑, 接下来看看wiki上的标准实现方法
 
